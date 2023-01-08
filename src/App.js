fe57936ee52ef3359f65cb618b7a354641f875aa
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-01-08 20:21:30 +0100

    Add homepage with heroimage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLightbulb} from '@fortawesome/free-solid-svg-icons';
+import React from 'react'
+import style from '../styles/Home.module.css'
+
+function Home() {
+  return (
+    <>  
+    <div className={`p-0 text-center bg-image hero-image ${style.heroImage}`} >
+        <div className={style.mask}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'><FontAwesomeIcon icon={faLightbulb} className="me-4 pb-1" />Vi lyser upp din vardag</h1>
+              <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
+                Kontakta oss
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
