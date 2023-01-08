c22cb9511737b87262af4b7f3cc3dd6629299211
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-01-08 20:50:23 +0100

    Add mediaqueries for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Navbar, Nav, Container, Image} from 'react-bootstrap';
-import logo from '../assets//images/install-logo.png';
+import { Navbar, Nav, Container} from 'react-bootstrap';
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">
-            <h1>Install Elteknik</h1>
+        <Navbar.Brand href="#home" className="mx-auto ms-sm-0">
+            <h1 className='text-wrap'>Install Elteknik</h1>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav"  className='mx-auto me-md-0'/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Hem</Nav.Link>
