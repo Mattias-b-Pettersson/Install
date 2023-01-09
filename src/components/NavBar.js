dcc53239c54032c3a3ec1f0eb3ee0555d6cfc590
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-01-09 18:24:57 +0100

    Update brandlogo color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Navbar, Nav, Container} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import style from "../styles/NavBar.module.css"
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg">
       <Container>
         <Navbar.Brand className="mx-auto ms-sm-0 me-sm-5">
-            <h1 className='text-wrap'>Install Elteknik</h1>
+            <h1 className={`text-wrap ${style.brandName}`}>Install Elteknik</h1>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"  className='mx-auto me-md-0'/>
         <Navbar.Collapse id="basic-navbar-nav">
