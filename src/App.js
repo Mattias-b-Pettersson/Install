f6d301a52305744a206dc83cf2d8ce69a701c389
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-01-09 11:22:00 +0100

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
@@ -7,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Home />
+        <Routes>
+          <Route exact="true" path="/install" element={<Home />} />
+        </Routes>
     </div>
   );
 }
