a9220212c99cf873738e9b046e50e544cffddd36
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-01-11 20:29:46 +0100

    Add container to carousel and main text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,38 +4,40 @@ import React from 'react'
 import style from '../styles/Home.module.css'
 import { Link } from 'react-router-dom';
 import Carousels from '../components/Carousel';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 
 function Home() {
   return (
     <>  
-    <div className={`p-0 text-center bg-image hero-image ${style.heroImage}`} >
-        <div className={style.mask}>
-          <div className='d-flex justify-content-center align-items-center h-100'>
-            <div className='text-white'>
-              <h1 className='mb-3'><FontAwesomeIcon icon={faLightbulb} className="me-4 pb-1 d-none d-md-inline" />Vi lyser upp din vardag</h1>
-              <Link className={`btn btn-outline-light btn-lg ${style.homeButton}`} to="/install/kontakt" role='button'>
-                Kontakta oss
-              </Link>
+      <div className={`p-0 text-center bg-image hero-image ${style.heroImage}`} >
+          <div className={style.mask}>
+            <div className='d-flex justify-content-center align-items-center h-100'>
+              <div className='text-white'>
+                <h1 className='mb-3'><FontAwesomeIcon icon={faLightbulb} className="me-4 pb-1 d-none d-md-inline" />Vi lyser upp din vardag</h1>
+                <Link className={`btn btn-outline-light btn-lg ${style.homeButton}`} to="/install/kontakt" role='button'>
+                  Kontakta oss
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    <Row className={`mx-0 pb-4 ${style.orangeGradientBg} d-flex justify-content-center`}>
-      <Col xs={10} md={5} xl={6} className='d-flex align-items-center '>
-        <p className="px-md-5 text-center my-5 lead">
-          Install Elteknik, baserat i Stockholm, erbjuder ett brett utbud av elektriska tjänster för både bostäder och kommersiella fastigheter.
-          Från installationer och reparationer till underhåll och besiktningar har vi kunskapen och expertisen att hantera alla dina elektriska behov.
-          Vi är stolta över vår noggrannhet, tidsenlig service och engagemang för kundnöjdhet.
-          Vårt team av certifierade och erfarna elektriker är alltid redo att hjälpa dig med alla elektriska problem du kan stå inför.
-          Kontakta oss idag för alla dina elektriska behov i Stockholm och omgivande områden.
-        </p>
-      </Col>
-      <Col xs={10} md={5} xl={3} className=''>
-        <Carousels />
-      </Col>
-    </Row>
+      <Container>
+      <Row className={`mx-0 pb-4 ${style.orangeGradientBg} d-flex justify-content-center`}>
+        <Col xs={10} lg={6} className='d-flex align-items-center '>
+          <p className="px-md-5 text-center my-5 lead">
+            Install Elteknik, baserat i Stockholm, erbjuder ett brett utbud av elektriska tjänster för både bostäder och kommersiella fastigheter.
+            Från installationer och reparationer till underhåll och besiktningar har vi kunskapen och expertisen att hantera alla dina elektriska behov.
+            Vi är stolta över vår noggrannhet, tidsenlig service och engagemang för kundnöjdhet.
+            Vårt team av certifierade och erfarna elektriker är alltid redo att hjälpa dig med alla elektriska problem du kan stå inför.
+            Kontakta oss idag för alla dina elektriska behov i Stockholm och omgivande områden.
+          </p>
+        </Col>
+        <Col xs={10} md={6} xl={5} className=''>
+          <Carousels />
+        </Col>
+      </Row>
+      </Container>
     </>
   )
 }
