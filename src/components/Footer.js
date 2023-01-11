58c112f4846cd414ff5ed63cf63633e60825c595
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-01-11 21:42:54 +0100

    Add grid layout for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import { faSquareFacebook, faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import { Container, Image } from 'react-bootstrap'
+import { Col, Container, Image, Row } from 'react-bootstrap'
 import styles from "../styles/Footer.module.css"
 import kollaelforetaget from '../assets/logos/logotyp-kollaelforetaget-webb.png' 
 
@@ -9,21 +9,29 @@ import kollaelforetaget from '../assets/logos/logotyp-kollaelforetaget-webb.png'
 const Footer = () => {
   return (
     <footer className="text-muted">
-    <Container className='d-flex border-top border-1 py-4'>
-        <p className='mx-0 fs-5'>
-            © 2021 Install Elteknik AB
-        </p>
-        <div className='mx-auto'>
-            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faSquareFacebook} className={`${styles.icon} fs-2 mx-1`} />
-            </a>
-            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faInstagramSquare} className={`${styles.icon} fs-2 mx-1`} />
-            </a>
-        </div>
-        <a href='https://www.elsakerhetsverket.se/kollaelforetaget/foretagsregister/?foretag=11069129&sok=1' target="_blank" rel="noreferrer">
-                <Image src={kollaelforetaget} height="40" className='p-0 m-0 mx-1 d-inline'/>
-          </a>
+    <Container className='border-top border-1 py-4'>
+        <Row>
+            <Col xs={12} md={4} className='d-flex flex-column justify-content-center align-items-center'>
+                <p className='m-0 fs-5 d-inline'>
+                    © 2021 Install Elteknik AB
+                </p>
+            </Col>
+            <Col xs={12} md={4} className='d-flex flex-column justify-content-center align-items-center'>
+                <div className=''>
+                    <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faSquareFacebook} className={`${styles.icon} fs-2 mx-1`} />
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faInstagramSquare} className={`${styles.icon} fs-2 mx-1`} />
+                    </a>
+                </div>
+            </Col>
+            <Col xs={12} md={4} className='d-flex flex-column justify-content-center align-items-center'>
+                <a href='https://www.elsakerhetsverket.se/kollaelforetaget/foretagsregister/?foretag=11069129&sok=1' target="_blank" rel="noreferrer">
+                    <Image src={kollaelforetaget} height="40" className='p-0 m-0 mx-1 d-inline'/>
+                </a>
+            </Col>
+          </Row>
     </Container>
 </footer>
   )
