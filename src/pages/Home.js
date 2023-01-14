a5e5995202c8e650614af9aa93b01995a5d5d622
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-01-14 13:26:59 +0100

    Add text to home page in 4 cols

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLightbulb} from '@fortawesome/free-solid-svg-icons';
+import { faLightbulb, faBolt} from '@fortawesome/free-solid-svg-icons';
 import React from 'react'
 import style from '../styles/Home.module.css'
 import { Link } from 'react-router-dom';
@@ -23,20 +23,59 @@ function Home() {
           </div>
         </div>
       <Container>
-      <Row className={`mx-0 pb-4 ${style.orangeGradientBg} d-flex justify-content-center`}>
-        <Col xs={10} lg={6} className='d-flex align-items-center '>
-          <p className="px-md-5 text-center my-5 lead">
-            Install Elteknik, baserat i Stockholm, erbjuder ett brett utbud av elektriska tjänster för både bostäder och kommersiella fastigheter.
-            Från installationer och reparationer till underhåll och besiktningar har vi kunskapen och expertisen att hantera alla dina elektriska behov.
-            Vi är stolta över vår noggrannhet, tidsenlig service och engagemang för kundnöjdhet.
-            Vårt team av certifierade och erfarna elektriker är alltid redo att hjälpa dig med alla elektriska problem du kan stå inför.
-            Kontakta oss idag för alla dina elektriska behov i Stockholm och omgivande områden.
-          </p>
-        </Col>
-        <Col xs={10} md={6} xl={5} className=''>
-          <Carousels />
-        </Col>
-      </Row>
+        <Row className='mt-3'>
+          <Col md={6} lg={3}>
+            <h3 className='d-block'>
+              <FontAwesomeIcon icon={faBolt} className="me-1" />
+              Värmegolv
+            </h3>
+            <p className='text-muted'>
+              Upplev lyxen av varma golv med våra tjänster för installation av uppvärmt golv. Våra expertelektriker kommer att arbeta med dig för att designa en anpassad lösning för uppvärmt golv som möter dina behov och budget.
+            </p>
+          </Col>
+          <Col md={6} lg={3}>
+            <h3 className='d-block'>
+              <FontAwesomeIcon icon={faBolt} className="me-1" />
+              Badrum
+            </h3>
+            <p className='text-muted'>
+              Uppgradera ditt badrum med våra professionella tjänster för badrumsinstallation. Våra elektriker är erfarna inom alla aspekter av badrumsinstallation, inklusive belysning, ventilation och elarbete.
+            </p>
+          </Col>
+          <Col md={6} lg={3}>
+            <h3 className='d-block'>
+              <FontAwesomeIcon icon={faBolt} className="me-1" />
+              Laddbox
+            </h3>
+            <p className='text-muted'>
+              Håll din elbil laddad och redo att köra med våra tjänster för installation av laddbox. Våra elektriker kommer att installera en säker och pålitlig laddbox i ditt hem eller företag, så att din EV alltid är redo för din nästa resa.
+            </p>
+          </Col>
+          <Col md={6} lg={3}>
+            <h3 className='d-block'>
+              <FontAwesomeIcon icon={faBolt} className="me-1" />
+              Elsäkerhet
+            </h3>
+            <p className='text-muted'>
+              Håll ditt hem eller företag säkert med våra tjänster för el-säkerhet.
+             Våra elektriker kommer att utföra en grundlig inspektion av din elektriska anläggning och göra eventuella nödvändiga reparationer eller uppgraderingar för att säkerställa att den möter alla säkerhetsstandarder.
+            </p>
+          </Col>
+        </Row>
+        <Row className={`mx-0 pb-4 ${style.orangeGradientBg} d-flex justify-content-center`}>
+          <Col xs={12} md={6} className='order-md-2'>
+            <Carousels />
+          </Col>
+          <Col xs={12} md={6} className='d-flex align-items-center '>
+            <p className="text-center lead mt-3">
+              Install Elteknik, baserat i Stockholm, erbjuder ett brett utbud av elektriska tjänster för både bostäder och kommersiella fastigheter.
+              Från installationer och reparationer till underhåll och besiktningar har vi kunskapen och expertisen att hantera alla dina elektriska behov.
+              Vi är stolta över vår noggrannhet, tidsenlig service och engagemang för kundnöjdhet.
+              Vårt team av certifierade och erfarna elektriker är alltid redo att hjälpa dig med alla elektriska problem du kan stå inför.
+              Kontakta oss idag för alla dina elektriska behov i Stockholm och omgivande områden.
+            </p>
+          </Col>
+        </Row>
       </Container>
     </>
   )
